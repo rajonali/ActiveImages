f9aca68d54dedc9b1e89366ab08abf1c32b4140f
Rajon Ali <rajonali44492@icloud.com>
2019-05-28 16:13:20 -0500

    styled first home page component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,18 @@ import Layout from '../components/layout';
 
 import '../styles/index.scss'
 
+import placeholder from '../images/placeholder.jpg';
+
+
+
 const HomePage = () => { return(
 <Layout>
-<p>Hello from home!</p>
+<div style={{backgroundImage: `url(${placeholder})`,overflow: 'hidden', height:'500px'}}> 
+
+<h1 style={{color:'white', fontWeight:'bold', fontSize:'50px', fontFamily:'helvetica', margin:'150px 0px 0px 150px', }}>ACTIVE IMAGE MEDIA</h1>
+<h4 style={{color:'white', fontWeight:'normal', fontFamily:'helvetica', margin:'10px 0px 0px 150px' }}>▶️ Watch Showreel</h4>
+
+</div>
 </Layout>) }
 
 
